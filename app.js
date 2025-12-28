14089fde0a35243aff1bc509af270a72504199bc
ather ali <a.ngaming102734@gmail.com>
2025-12-29 02:04:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// alert('Asalma walikum');
+
+// var ansWer =prompt ('Answer the Question?')
+
+// if (ansWer === 'Walikum Asalam'.toLowerCase())  {
+//    alert('Thank you');
+//    alert('Ke app ne Salam ka Jawab dia.')
+//    alert('You are Answer my Question  '+' Thank you')
+// }else {
+//     alert('Your non-muslim');
+// }
+
+// alert('Your non-muslim');
+
+// var Data = prompt('Ok you are Non-muslim ?');
+
+// if (Data === 'Yes'.toLowerCase) {
+//     alert('Hello');
+//     alert('Thank you')
+// }else{
+//     alert('You are repeat the Page')
+// }
